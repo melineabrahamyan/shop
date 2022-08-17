b2fd9bf2ad066518e98eb23ca5356a51ce899d48
melineabrahamyan <abrahamyan.meline@gmail.com>
2022-08-17 15:10:32 +0400

    added data from papapizza.am

---
+++ data.js
@@ -0,0 +1,362 @@
+export let data = [
+  {
+    title: "Կոմբո-1",
+    img: "https://static.insales-cdn.com/images/products/1/6570/428317098/large_%D5%A4%D5%BF.jpg",
+    price: "8200 դր",
+  },
+  {
+    title: "Կոմբո 2",
+    img: "https://static.insales-cdn.com/images/products/1/4919/428290871/large_T4.jpg",
+    price: "7100 դր",
+  },
+  {
+    title: "Կոմբո 3",
+    img: "https://static.insales-cdn.com/images/products/1/2984/428297128/large_PaPa-Pizza-Kombo-3.jpg",
+    price: "5900 դր",
+  },
+  {
+    title: "Կոմբո 4",
+    img: "https://static.insales-cdn.com/images/products/1/6366/428300510/large_PaPa-Pazza--Kombo-4.jpg",
+    price: "6100 դր",
+  },
+  {
+    title: "Կոմբո 5",
+    img: "https://static.insales-cdn.com/images/products/1/7790/428318318/large_kombo-5.jpg",
+    price: "8200 դր",
+  },
+  {
+    title: "Կոմբո 6",
+    img: "https://static.insales-cdn.com/images/products/1/4530/428315058/large_4x.jpg",
+    price: "10900 դր",
+  },
+  {
+    title: "ՊաՊա",
+    img: "https://static.insales-cdn.com/images/products/1/1511/219579879/large_8.jpg",
+    price: "2200 դր — 3800 դր",
+  },
+  {
+    title: "Պեպպերոնի",
+    img: "https://static.insales-cdn.com/images/products/1/1576/219579944/large_3.jpg",
+    price: "2000 դր — 3500 դր",
+  },
+  {
+    title: "Մարգարիտա",
+    img: "https://static.insales-cdn.com/images/products/1/7817/491863689/large_thumbnail.jpg",
+    price: "1700 դր — 2800 դր",
+  },
+  {
+    title: "Իսպանական",
+    img: "https://static.insales-cdn.com/images/products/1/5069/428299213/large_71762117_505242246966001_5722880124752232448_n.jpg",
+    price: "2400 դր — 4400 դր",
+  },
+  {
+    title: "Պեպպերոնի - կծու",
+    img: "https://static.insales-cdn.com/images/products/1/1637/219580005/large_31.jpg",
+    price: "2100 դր — 3700 դր",
+  },
+  {
+    title: "Մեքսիկական",
+    img: "https://static.insales-cdn.com/images/products/1/2217/219580585/large_17.jpg",
+    price: "2000 դր — 3500 դր",
+  },
+  {
+    title: "Ասորտի",
+    img: "https://static.insales-cdn.com/images/products/1/6292/294451348/large_%D4%B1%D5%BD%D5%B8%D6%80%D5%BF%D5%AB1.jpg",
+    price: "2200 դր — 3800 դր",
+  },
+  {
+    title: "Հավ խոզապուխտ",
+    img: "https://static.insales-cdn.com/images/products/1/7676/424254972/large_thumbnail.jpg",
+    price: "2200 դր — 3600 դր",
+  },
+  {
+    title: "Կրկնակի Պեպպերոնի",
+    img: "https://static.insales-cdn.com/images/products/1/2024/219580392/large_16.jpg",
+    price: "2400 դր — 4200 դր",
+  },
+  {
+    title: "4x4 պիցցա",
+    img: "https://static.insales-cdn.com/images/products/1/2269/219580637/large_9.jpg",
+    price: "3100 դր — 4100 դր",
+  },
+  {
+    title: "4 Պանիր",
+    img: "https://static.insales-cdn.com/images/products/1/2294/219408630/large_image1.JPG",
+    price: "2600 դր — 4300 դր",
+  },
+  {
+    title: "Ամերիկանո",
+    img: "https://static.insales-cdn.com/images/products/1/5753/418256505/large_54654654.jpg",
+    price: "2000 դր — 3500 դր",
+  },
+  {
+    title: "Ամառային",
+    img: "https://static.insales-cdn.com/images/products/1/2251/234137803/large_image1__2_.JPG",
+    price: "2000 դր — 3200 դր",
+  },
+  {
+    title: "Պեպպերոնի-լոլիկ",
+    img: "https://static.insales-cdn.com/images/products/1/4416/247435584/large___%D0%94%D0%BE%D0%B4%D0%BE_%D0%9F%D0%B8%D1%86%D1%86%D0%B0_%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0___%D0%94%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D0%B0_%D0%BF%D0%B8%D1%86%D1%86%D1%8B_%D0%B7%D0%B0_60_%D0%BC%D0%B8%D0%BD%D1%83%D1%82_%D0%B8%D0%BB%D0%B8_%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE._%D0%97%D0%B2%D0%BE%D0%BD%D0%BE%D0%BA_%D0%BF%D0%BE_%D1%82%D0%B5%D0%BB%D0%B5%D1%84%D0%BE%D0%BD%D1%83_8_800_302-00-.png",
+    price: "2200 դր — 3700 դր",
+  },
+  {
+    title: "Սուպեր մսային",
+    img: "https://static.insales-cdn.com/images/products/1/1722/219580090/large_13.jpg",
+    price: "2200 դր — 3900 դր",
+  },
+  {
+    title: "Իտալական",
+    img: "https://static.insales-cdn.com/images/products/1/2559/219580927/large_7.jpg",
+    price: "2100 դր — 3800 դր",
+  },
+  {
+    title: "ՀավՊանիր",
+    img: "https://static.insales-cdn.com/images/products/1/2657/219581025/large_29.jpg",
+    price: "2100 դր — 3500 դր",
+  },
+  {
+    title: "Սունկ զեյթուն",
+    img: "https://static.insales-cdn.com/images/products/1/4243/220450963/large_%D5%8D%D5%B8%D6%82%D5%B6%D5%AF_%D5%A6%D5%A5%D5%B5%D5%A9%D5%B8%D6%82%D5%B6.jpg",
+    price: "2300 դր — 2900 դր",
+  },
+  {
+    title: "Հավ BBQ սոուսով",
+    img: "https://static.insales-cdn.com/images/products/1/7969/355966753/large_img_1397.jpg",
+    price: "2200 դր — 3900 դր",
+  },
+  {
+    title: "Կրկնակի մոցարելլա",
+    img: "https://static.insales-cdn.com/images/products/1/4752/220451472/large_18.png",
+    price: "3300 դր — 3900 դր",
+  },
+  {
+    title: "Բանջարեղենային",
+    img: "https://static.insales-cdn.com/images/products/1/2451/219580819/large_19.jpg",
+    price: "1700 դր — 2900 դր",
+  },
+  {
+    title: "Վիտչինա սունկ",
+    img: "https://static.insales-cdn.com/images/products/1/2477/219580845/large_12.jpg",
+    price: "2100 դր — 3500 դր",
+  },
+  {
+    title: "Բեկոն",
+    img: "https://static.insales-cdn.com/images/products/1/2501/219580869/large_10.jpg",
+    price: "2200 դր — 3900 դր",
+  },
+  {
+    title: "Խոզապուխտ պանիր",
+    img: "https://static.insales-cdn.com/images/products/1/4532/220451252/large_%D4%BD%D5%B8%D5%A6%D5%A1%D5%BA%D5%B8%D6%82%D5%AD%D5%BF_%D5%BA%D5%A1%D5%B6%D5%AB%D6%80.jpg",
+    price: "1900 դր — 3400 դր",
+  },
+  {
+    title: "Մսային",
+    img: "https://static.insales-cdn.com/images/products/1/2596/219580964/large_24.jpg",
+    price: "2000 դր — 3500 դր",
+  },
+  {
+    title: "Հավայան խոզապուխտով",
+    img: "https://static.insales-cdn.com/images/products/1/4942/220451662/large_22.jpg",
+    price: "2900 դր — 3600 դր",
+  },
+  {
+    title: "Պանրային",
+    img: "https://static.insales-cdn.com/images/products/1/2702/219581070/large_25.jpg",
+    price: "1800 դր — 3100 դր",
+  },
+  {
+    title: "Հավայան",
+    img: "https://static.insales-cdn.com/images/products/1/2768/219581136/large_5.jpg",
+    price: "2100 դր — 3700 դր",
+  },
+  {
+    title: "Հավ սունկ",
+    img: "https://static.insales-cdn.com/images/products/1/2787/219581155/large_29.jpg",
+    price: "2200 դր — 3600 դր",
+  },
+  {
+    title: "Բեկոն-Խոզապուխտ",
+    img: "https://static.insales-cdn.com/images/products/1/4622/219582990/large_34.jpg",
+    price: "2200 դր — 3600 դր",
+  },
+  {
+    title: "Պիցցա Որսորդական",
+    img: "https://static.insales-cdn.com/images/products/1/3926/219582294/large_33.jpg",
+    price: "1900 դր — 2900 դր",
+  },
+  {
+    title: "Մանկական կոմբո 1",
+    img: "https://static.insales-cdn.com/images/products/1/891/417645435/large_Mank_Combo_1.png",
+    price: "3650 դր — 3650 դր",
+  },
+  {
+    title: "Մանկական կոմբո 2",
+    img: "https://static.insales-cdn.com/images/products/1/6333/417650877/large_Mank_Combo_2.png",
+    price: "4950 դր — 4950 դր",
+  },
+  {
+    title: "Կարտոֆիլի ֆրի",
+    img: "https://static.insales-cdn.com/images/products/1/798/427926302/large_deb__1_.jpg",
+    price: "550 դր",
+  },
+  {
+    title: "Կարտոֆիլի ֆրի + օրեգանո",
+    img: "https://static.insales-cdn.com/images/products/1/948/427926452/large_deb__1_.jpg",
+    price: "650 դր",
+  },
+  {
+    title: "Գյուղական կարտոֆիլ",
+    img: "https://static.insales-cdn.com/images/products/1/130/427925634/large_adee.jpg",
+    price: "650 դր",
+  },
+  {
+    title: "Գյուղական կարտոֆիլ + օրեգանո",
+    img: "https://static.insales-cdn.com/images/products/1/215/427925719/large_adee.jpg",
+    price: "750 դր",
+  },
+  {
+    title: "Կարտոֆիլի ֆրի + պարմեզան",
+    img: "https://static.insales-cdn.com/images/products/1/2951/285567879/large_%D0%BF%D0%B0%D1%80%D0%BC%D0%B5%D0%B7%D0%B0%D0%BD-%D1%84%D1%80%D0%B8.jpg",
+    price: "950 դր",
+  },
+  {
+    title: "Հավի նագեթներ",
+    img: "https://static.insales-cdn.com/images/products/1/4318/420327646/large_yli.jpg",
+    price: "1150 դր",
+  },
+  {
+    title: "Հունական աղցան",
+    img: "https://static.insales-cdn.com/images/products/1/5334/330478806/large_13_60348965_%D0%93%D0%A0%D0%95%D0%A7%D0%95%D0%A1%D0%9A%D0%98%D0%99-%D0%A1%D0%90%D0%9B%D0%90%D0%A2.png",
+    price: "1300 դր",
+  },
+  {
+    title: "Կապրեզե աղցան",
+    img: "https://static.insales-cdn.com/images/products/1/5737/330479209/large_5cafd5aa5cde19931b116cf8feb37a87.jpg",
+    price: "1700 դր",
+  },
+  {
+    title: "Ամառային աղցան",
+    img: "https://static.insales-cdn.com/images/products/1/8137/418275273/large_%D4%B1%D5%B4%D5%A1%D5%BC%D5%A1%D5%B5%D5%AB%D5%B6_%D5%A1%D5%B2%D6%81%D5%A1%D5%B6.jpg",
+    price: "900 դր",
+  },
+  {
+    title: "Ռոլլ հավով",
+    img: "https://static.insales-cdn.com/images/products/1/6105/453924825/large_a859c64eaab97bc9dca666a663c8357b.jpg",
+    price: "1700 դր",
+  },
+  {
+    title: "Ռոլլ հավով և բեկոնով",
+    img: "https://static.insales-cdn.com/images/products/1/6992/453925712/large_a859c64eaab97bc9dca666a663c8357b.jpg",
+    price: "1800 դր",
+  },
+  {
+    title: "Հավ-Խոզապուխտ",
+    img: "https://static.insales-cdn.com/images/products/1/4640/149140000/large_992db008-4561-4988-a118-0a1b2edd68d6.jpg",
+    price: "1800 դր",
+  },
+  {
+    title: "Հավ-Բեկոն",
+    img: "https://static.insales-cdn.com/images/products/1/4792/149140152/large_992db008-4561-4988-a118-0a1b2edd68d6.jpg",
+    price: "1800 դր",
+  },
+  {
+    title: "Պանրային սոուս",
+    img: "https://static.insales-cdn.com/images/products/1/4353/251810049/large_22-13-things-your-pizza-guy-wont-tell-you-knocking.jpg",
+    price: "300 դր",
+  },
+  {
+    title: "Չիլի սոուս",
+    img: "https://static.insales-cdn.com/images/products/1/6223/251811919/large_55555.jpg",
+    price: "350 դր",
+  },
+  {
+    title: "Սխտորի սոուս",
+    img: "https://static.insales-cdn.com/images/products/1/7159/251812855/large_685428-a-pizza-delivery-man-holding-a-hot-fresh-pepperoni-pizza-isolated-on-white-.jpg",
+    price: "300 դր",
+  },
+  {
+    title: "Կեսար սոուս",
+    img: "https://static.insales-cdn.com/images/products/1/7775/251813471/large_10404233_868564066490159_2234506626126543322_n.jpg",
+    price: "300 դր",
+  },
+  {
+    title: "Կետչուպ",
+    img: "https://static.insales-cdn.com/images/products/1/147/251814035/large_68934446_468645103715736_7921924666162151424_n.jpg",
+    price: "200 դր",
+  },
+  {
+    title: "ՊաՊա սոուս",
+    img: "https://static.insales-cdn.com/images/products/1/1012/251814900/large_72086176_743921416053157_9122723622916456448_n.jpg",
+    price: "350 դր",
+  },
+  {
+    title: "Հալապենյո (կծու)",
+    img: "https://static.insales-cdn.com/images/products/1/1995/251815883/large_88358375-delivery-guy-pizza-fast-food-icon-image-vector-illustration-design.jpg",
+    price: "350 դր",
+  },
+  {
+    title: "Իսպանական կտրատած ձիթապտուղ",
+    img: "https://static.insales-cdn.com/images/products/1/3200/251817088/large_Add_a_heading.png",
+    price: "300 դր",
+  },
+  {
+    title: "Կոկա-Կոլա 0,5լ",
+    img: "https://static.insales-cdn.com/images/products/1/7630/135232974/large_xCoca-Cola.jpg.pagespeed.ic.81M7pAzwA1.jpg",
+    price: "500 դր",
+  },
+  {
+    title: "Ֆանտա 0,5լ",
+    img: "https://static.insales-cdn.com/images/products/1/7243/424262731/large_1553523596-4735.jpg",
+    price: "500 դր",
+  },
+  {
+    title: "Սպրայթ 0,5լ",
+    img: "https://static.insales-cdn.com/images/products/1/924/122839964/large_download.jpg",
+    price: "500 դր",
+  },
+  {
+    title: "Կոկա-Կոլա 0,25լ",
+    img: "https://static.insales-cdn.com/images/products/1/5895/198137607/large_Cola_0.25.png",
+    price: "400 դր",
+  },
+  {
+    title: "Ֆանտա 0,25լ",
+    img: "https://static.insales-cdn.com/images/products/1/7169/198138881/large_Fanta_0.25.jpg",
+    price: "400 դր",
+  },
+  {
+    title: "Սփրայթ 0,25լ",
+    img: "https://static.insales-cdn.com/images/products/1/7232/198138944/large_Sprite_0.25.jpg",
+    price: "400 դր",
+  },
+  {
+    title: "ԹԱՄԱՐԱ - Խնձորի",
+    img: "https://static.insales-cdn.com/images/products/1/66/148996162/large_4066_xndzor-1L-Nektars.jpg",
+    price: "350 դր — 950 դր",
+  },
+  {
+    title: "ԹԱՄԱՐԱ - Դեղձի",
+    img: "https://static.insales-cdn.com/images/products/1/8181/148996085/large_4581_deghc-1L-Nektars.jpg",
+    price: "350 դր — 950 դր",
+  },
+  {
+    title: "ԹԱՄԱՐԱ - Բալի",
+    img: "https://static.insales-cdn.com/images/products/1/8086/148995990/large_4136_bal-1L-Nektars.jpg",
+    price: "350 դր — 950 դր",
+  },
+  {
+    title: "ԹԱՄԱՐԱ - Ծիրանի",
+    img: "https://static.insales-cdn.com/images/products/1/7975/148995879/large_3948_ciran-1L-Nektars.jpg",
+    price: "350 դր — 950 դր",
+  },
+  {
+    title: "Ջուր Բյուրեղ 0,5լ",
+    img: "https://static.insales-cdn.com/images/products/1/4534/309522870/large_1111581501612_byuregh1.5lactive.png",
+    price: "250 դր",
+  },
+  {
+    title: "Ջերկուկ 0,5լ",
+    img: "https://static.insales-cdn.com/images/products/1/4648/419303976/large_1579786056-3726.jpg",
+    price: "250 դր",
+  },
+];
